763eb557c527ea6ece35c0e39f9291ad8b7284c2
Excidio <popov095@gmail.com>
2017-04-24 19:34:23 +0500

    wwwroot changes

--- src/WebApp/wwwroot/app/url-record.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var UrlRecord = (function () {
-    function UrlRecord() {
-    }
-    return UrlRecord;
-}());
-exports.UrlRecord = UrlRecord;
-//# sourceMappingURL=url-record.js.map
